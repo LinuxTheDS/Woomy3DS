f64d754fb74656e93e10e9883de9f45bbc640953
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2022-09-21 20:06:07 -0500

    Very Woomy And Vemmo

--- arm11/source/types.h
+++ arm11/source/types.h
@@ -1,27 +1,5 @@
 /*
-*   This file is part of Luma3DS
-*   Copyright (C) 2016-2020 Aurora Wright, TuxSH
-*
-*   This program is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
-*       * Requiring preservation of specified reasonable legal notices or
-*         author attributions in that material or in the Appropriate Legal
-*         Notices displayed by works containing it.
-*       * Prohibiting misrepresentation of the origin of that material,
-*         or requiring that modified versions of such material be marked in
-*         reasonable ways as different from the original version.
+*  as different from the original version.
 */
 
 #pragma once
@@ -29,7 +7,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-//Common data types
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
